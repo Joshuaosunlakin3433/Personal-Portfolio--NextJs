1d8510a7ffda2d7857136a2683a867f5084c3335
Joshuaosunlakin3433 <64253576+Joshuaosunlakin3433@users.noreply.github.com>
2025-05-05 00:47:11 +0100

    created an HeroOrbit component for stars positioning

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -3,20 +3,45 @@ import Image from "next/image";
 import ArrowDown from "@/assets/icons/arrow-down.svg";
 import grainImage from "@/assets/images/grain.jpg";
 import StarIcon from "@/assets/icons/star.svg";
+import { HeroOrbit } from "@/components/HeroOrbit";
+import SparkleIcon from "@/assets/icons/sparkle.svg";
 
 export const HeroSection = () => {
   return (
-    <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip"> 
-      <div className="absolute inset-0 -z-30 opacity-5" style={{
-        backgroundImage: `url(${grainImage.src})`,
-      }}></div>
+    <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
+      <div
+        className="absolute inset-0 -z-30 opacity-5"
+        style={{
+          backgroundImage: `url(${grainImage.src})`,
+        }}
+      ></div>
       <div className="size-[620px] hero-ring animate-pulse"></div>
       <div className="size-[820px] hero-ring"></div>
       <div className="size-[1020px] hero-ring"></div>
       <div className="size-[1220px] hero-ring"></div>
-      <div >
-        <StarIcon/>
-      </div>
+      <HeroOrbit size={800} rotation={-72}>
+        <StarIcon className="size-28 text-emerald-300" />
+      </HeroOrbit>
+
+      <HeroOrbit size={550} rotation={20}>
+        <StarIcon className="size-12 text-emerald-300" />
+      </HeroOrbit>
+
+      <HeroOrbit size={590} rotation={98}>
+        <StarIcon className="size-8 text-emerald-300" />
+      </HeroOrbit>
+      <HeroOrbit size={430} rotation={-14}>
+        <SparkleIcon className="size-8 text-emerald-300/20" />
+      </HeroOrbit>
+
+      <HeroOrbit size={430} rotation={-14}>
+        <SparkleIcon className="size-8 text-emerald-300/20" />
+      </HeroOrbit>
+
+      <HeroOrbit size={440} rotation = {79}>
+        <SparkleIcon className="size-5 text-emerald-300/20" />
+      </HeroOrbit>
+      
       <div className="container">
         <div className="flex flex-col items-center">
           <Image
@@ -32,19 +57,19 @@ export const HeroSection = () => {
           </div>
         </div>
         <div className="max-w-lg mx-auto">
-        <h1 className="font-calistoga font-bold text-3xl md:text-5xl text-center mt-8 tracking-wide">
-          Building Exceptional User Experiences
-        </h1>
-        <p className="text-center mt-4 text-white/50 md:text-lg">
-          I specialize in transforming designs into functional, high-perforning
-          web applications. Let's discuss your discuss your next project
-        </p>
+          <h1 className="font-calistoga font-bold text-3xl md:text-5xl text-center mt-8 tracking-wide">
+            Building Exceptional User Experiences
+          </h1>
+          <p className="text-center mt-4 text-white/50 md:text-lg">
+            I specialize in transforming designs into functional,
+            high-perforning web applications. Let's discuss your discuss your
+            next project
+          </p>
         </div>
         <div className="flex flex-col md:flex-row md:justify-center items-center mt-8 gap-4">
           <button className="inline-flex items-center gap-2 capitalize border border-white/15 h-12 px-6 rounded-xl">
-            
             <span className="font-semibold">Explore my work</span>
-            <ArrowDown className = "size-4 animate-bounce"/>
+            <ArrowDown className="size-4 animate-bounce" />
           </button>
           <div className="inline-flex gap-2 items-center border border-white bg-white h-12 px-6 rounded-xl text-gray-900">
             <div className="text-xl animate-pulse">👋 </div>

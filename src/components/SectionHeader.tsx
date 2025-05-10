231aedd1bfa44d25d707c0b353c57e41c4fe5db3
Joshuaosunlakin3433 <64253576+Joshuaosunlakin3433@users.noreply.github.com>
2025-05-10 21:00:29 +0100

    feat: Implement SectionHeader component for structured section titles

---
+++ src/components/SectionHeader.tsx
@@ -0,0 +1,31 @@
+const SectionHeader = ({
+  title,
+  eyebrow,
+  description,
+}: {
+  title: string;
+  eyebrow: string;
+  description: string;
+}) => {
+  return (
+    <>
+      <div className="flex justify-center">
+        <p
+          className="tracking-widest font-semibold uppercase text-center
+           bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text 
+           text-transparent"
+        >
+          {title}
+        </p>
+      </div>
+      <h2 className="capitalize mt-6 text-3xl md:text-5xl font-calistoga text-center">
+        {eyebrow}
+      </h2>
+      <p className="font-sans mt-4 text-white/60 md:text-lg lg:text-xl max-w-md text-cente mx-auto text-center">
+        {description}
+      </p>
+    </>
+  );
+};
+ 
+export default SectionHeader;

3c0446bd453f189e6ad550eb61d533905c77834c
Joshuaosunlakin3433 <64253576+Joshuaosunlakin3433@users.noreply.github.com>
2025-07-22 23:20:15 +0100

    fix: Reorder footer links for better organization

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 
 const footerLinks = [
   {
-    title: "GitHub",
-    href: "https://github.com/Joshuaosunlakin3433",
-  },
-   {
     title: "Resume",
     href: "/JOSHUA-OSUNLAKIN-RESUME.pdf",
+  },
+  {
+    title: "GitHub",
+    href: "https://github.com/Joshuaosunlakin3433",
   },
   {
     title: "LinkedIn",
@@ -26,16 +26,23 @@ const footerLinks = [
 
 export const Footer = () => {
   return (
-    <footer  className="relative">
+    <footer className="relative">
       <div className="absolute -z-10 h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-teal-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)]"></div>
       <div className="container">
         <div className="border-t border-white/15 py-6 flex flex-col md:flex-row md:justify-between items-center text-sm gap-8 ">
           <div className="text-white/40 text-center">
-            &copy; {new Date().getFullYear()} Built by Dr C.J. All rights reserved.
+            &copy; {new Date().getFullYear()} Built by Dr C.J. All rights
+            reserved.
           </div>
           <nav className="flex flex-col md:flex-row items-center gap-8">
             {footerLinks.map((link) => (
-              <Link href={link.href} key={link.title} className="inline-flex items-center gap-1.5 cursor-pointer" target="_blank" rel="noopener noreferrer">
+              <Link
+                href={link.href}
+                key={link.title}
+                className="inline-flex items-center gap-1.5 cursor-pointer"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <span>{link.title}</span>
                 <ArrowUpRightIcon className="size-4" />
               </Link>
